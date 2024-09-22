5465ca978aeb1b7d289564519876d05a7ffe27c6
Mochizuki Ken <140779472+Mochizuki-Ken@users.noreply.github.com>
2024-09-22 12:05:25 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,12 +5,12 @@ import firebase from 'firebase/compat/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCDWUkiuc7y6vguL-RkH5SAO3i46Dq5Y3w",
-    authDomain: "christmas--card.firebaseapp.com",
-    projectId: "christmas--card",
-    storageBucket: "christmas--card.appspot.com",
-    messagingSenderId: "126985412846",
-    appId: "1:126985412846:web:b65e7a2b2b85444b8093ff"
+//     apiKey: "AIzaSyCDWUkiuc7y6vguL-RkH5SAO3i46Dq5Y3w",
+//     authDomain: "christmas--card.firebaseapp.com",
+//     projectId: "christmas--card",
+//     storageBucket: "christmas--card.appspot.com",
+//     messagingSenderId: "126985412846",
+//     appId: "1:126985412846:web:b65e7a2b2b85444b8093ff"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
